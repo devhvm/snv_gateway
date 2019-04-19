49cbbccda8ca2a56583082fd4c54ab20c3d1b435
VuNT <nguyenthachvu.vn@gmail.com>
2019-04-19 20:10:19 +0700

    update cors

--- src/main/java/com/manager/gateway/GatewayApp.java
+++ src/main/java/com/manager/gateway/GatewayApp.java
@@ -1,9 +1,11 @@
 package com.manager.gateway;
 
-import com.manager.gateway.config.ApplicationProperties;
-import com.manager.gateway.config.DefaultProfileUtil;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
 
-import io.github.jhipster.config.JHipsterConstants;
+import javax.annotation.PostConstruct;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -14,96 +16,117 @@ import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-import javax.annotation.PostConstruct;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.Collection;
+import com.manager.gateway.config.ApplicationProperties;
+import com.manager.gateway.config.DefaultProfileUtil;
+
+import io.github.jhipster.config.JHipsterConstants;
 
 @SpringBootApplication
-@EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class})
+@EnableConfigurationProperties({ LiquibaseProperties.class, ApplicationProperties.class })
 @EnableDiscoveryClient
 @EnableZuulProxy
 public class GatewayApp {
 
-    private static final Logger log = LoggerFactory.getLogger(GatewayApp.class);
+	private static final Logger log = LoggerFactory.getLogger(GatewayApp.class);
 
-    private final Environment env;
+	private final Environment env;
 
-    public GatewayApp(Environment env) {
-        this.env = env;
-    }
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 
-    /**
-     * Initializes gateway.
-     * <p>
-     * Spring profiles can be configured with a program argument --spring.profiles.active=your-active-profile
-     * <p>
-     * You can find more information on how profiles work with JHipster on <a href="https://www.jhipster.tech/profiles/">https://www.jhipster.tech/profiles/</a>.
-     */
-    @PostConstruct
-    public void initApplication() {
-        Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
-        if (activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_PRODUCTION)) {
-            log.error("You have misconfigured your application! It should not run " +
-                "with both the 'dev' and 'prod' profiles at the same time.");
-        }
-        if (activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_CLOUD)) {
-            log.error("You have misconfigured your application! It should not " +
-                "run with both the 'dev' and 'cloud' profiles at the same time.");
-        }
-    }
+	public GatewayApp(Environment env) {
+		this.env = env;
+	}
 
-    /**
-     * Main method, used to run the application.
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(GatewayApp.class);
-        DefaultProfileUtil.addDefaultProfile(app);
-        Environment env = app.run(args).getEnvironment();
-        logApplicationStartup(env);
-    }
+	/**
+	 * Initializes gateway.
+	 * <p>
+	 * Spring profiles can be configured with a program argument
+	 * --spring.profiles.active=your-active-profile
+	 * <p>
+	 * You can find more information on how profiles work with JHipster on <a href=
+	 * "https://www.jhipster.tech/profiles/">https://www.jhipster.tech/profiles/</a>.
+	 */
+	@PostConstruct
+	public void initApplication() {
+		Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+		if (activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+				&& activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_PRODUCTION)) {
+			log.error("You have misconfigured your application! It should not run "
+					+ "with both the 'dev' and 'prod' profiles at the same time.");
+		}
+		if (activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+				&& activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_CLOUD)) {
+			log.error("You have misconfigured your application! It should not "
+					+ "run with both the 'dev' and 'cloud' profiles at the same time.");
+		}
+	}
 
-    private static void logApplicationStartup(Environment env) {
-        String protocol = "http";
-        if (env.getProperty("server.ssl.key-store") != null) {
-            protocol = "https";
-        }
-        String serverPort = env.getProperty("server.port");
-        String contextPath = env.getProperty("server.servlet.context-path");
-        if (StringUtils.isBlank(contextPath)) {
-            contextPath = "/";
-        }
-        String hostAddress = "localhost";
-        try {
-            hostAddress = InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e) {
-            log.warn("The host name could not be determined, using `localhost` as fallback");
-        }
-        log.info("\n----------------------------------------------------------\n\t" +
-                "Application '{}' is running! Access URLs:\n\t" +
-                "Local: \t\t{}://localhost:{}{}\n\t" +
-                "External: \t{}://{}:{}{}\n\t" +
-                "Profile(s): \t{}\n----------------------------------------------------------",
-            env.getProperty("spring.application.name"),
-            protocol,
-            serverPort,
-            contextPath,
-            protocol,
-            hostAddress,
-            serverPort,
-            contextPath,
-            env.getActiveProfiles());
+	/**
+	 * Main method, used to run the application.
+	 *
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(GatewayApp.class);
+		DefaultProfileUtil.addDefaultProfile(app);
+		Environment env = app.run(args).getEnvironment();
+		logApplicationStartup(env);
+	}
 
-        String configServerStatus = env.getProperty("configserver.status");
-        if (configServerStatus == null) {
-            configServerStatus = "Not found or not setup for this application";
-        }
-        log.info("\n----------------------------------------------------------\n\t" +
-                "Config Server: \t{}\n----------------------------------------------------------", configServerStatus);
-    }
+	private static void logApplicationStartup(Environment env) {
+		String protocol = "http";
+		if (env.getProperty("server.ssl.key-store") != null) {
+			protocol = "https";
+		}
+		String serverPort = env.getProperty("server.port");
+		String contextPath = env.getProperty("server.servlet.context-path");
+		if (StringUtils.isBlank(contextPath)) {
+			contextPath = "/";
+		}
+		String hostAddress = "localhost";
+		try {
+			hostAddress = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			log.warn("The host name could not be determined, using `localhost` as fallback");
+		}
+		log.info(
+				"\n----------------------------------------------------------\n\t"
+						+ "Application '{}' is running! Access URLs:\n\t" + "Local: \t\t{}://localhost:{}{}\n\t"
+						+ "External: \t{}://{}:{}{}\n\t"
+						+ "Profile(s): \t{}\n----------------------------------------------------------",
+				env.getProperty("spring.application.name"), protocol, serverPort, contextPath, protocol, hostAddress,
+				serverPort, contextPath, env.getActiveProfiles());
+
+		String configServerStatus = env.getProperty("configserver.status");
+		if (configServerStatus == null) {
+			configServerStatus = "Not found or not setup for this application";
+		}
+		log.info(
+				"\n----------------------------------------------------------\n\t"
+						+ "Config Server: \t{}\n----------------------------------------------------------",
+				configServerStatus);
+	}
 }
